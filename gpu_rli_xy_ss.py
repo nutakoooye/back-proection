fix: back-proection gpu calculation second generation

gpu_rli_xy_ss.py
@@ -1,10 +1,12 @@
 from numba import cuda
+import numba as nb
 import numpy as np
 import math
+from utils import time_of_function
 
 
 @cuda.jit
-def kernel_2d_array(X1sint, Y1sint, dxsint, dysint, Xrls, Yrls, Zrls,
+def kernel_2d_array_1(X1sint, Y1sint, dxsint, dysint, Xrls, Yrls, Zrls,
            Vxrls, Vyrls, Vzrls, Tsint, Tr, Vrls, c, t2, T0, Kss,
            Uout01ss, Fs, WinSampl, Zxy1, lamda):
     nx, ny = cuda.grid(2)
@@ -18,8 +20,8 @@ def kernel_2d_array(X1sint, Y1sint, dxsint, dysint, Xrls, Yrls, Zrls,
         q0 = int((yt - Yrls[0]) / (Vyrls * Tr))  # индекс номера периода повторения для траверса
         q1 = q0 - int(Tsint / 2 / Tr)  # начальный индекс суммирования
         q2 = q0 + int(Tsint / 2 / Tr)  # конечный индекс суммирования
-        d0 = np.sqrt((Xrls[0] - xt) ** 2 + (
-                Zrls[0] - zt) ** 2)  # дальность на траверсе
+        d0 = ((Xrls[0] - xt) ** 2 + (
+                Zrls[0] - zt) ** 2) ** 0.5  # дальность на траверсе
 
         ar = Vrls ** 2 / d0  # радиальное ускорение для компенсации МД и МЧ
         # непосредственно суммирование - КН с компенсацией МД и МЧ
@@ -28,9 +30,9 @@ def kernel_2d_array(X1sint, Y1sint, dxsint, dysint, Xrls, Yrls, Zrls,
             # дальность между точкой синтезирования и РЛС в q-ом
             # периоде повторения
             # disp(q)
-            d = np.sqrt((xt - (Xrls[0] + Vxrls * q * Tr)) ** 2 +
+            d = ((xt - (Xrls[0] + Vxrls * q * Tr)) ** 2 +
                         (yt - (Yrls[0] + Vyrls * q * Tr)) ** 2 +
-                        (zt - (Zrls[0] + Vzrls * q * Tr)) ** 2)
+                        (zt - (Zrls[0] + Vzrls * q * Tr)) ** 2) ** 0.5
             # дробный номер отсчета, где находится сигнал, по быстрому времени
             ndr = (2 * d / c - t2 + T0) * Kss * Fs
             # целая и дробная часть индекса
@@ -41,19 +43,82 @@ def kernel_2d_array(X1sint, Y1sint, dxsint, dysint, Xrls, Yrls, Zrls,
                 n + 1, q] * drob
 
             # суммирование с учетом поворота фазы и взвешивания
-            sum1 = sum1 + ut * WinSampl[q - q1 + 1] * np.exp(
+            sum1 = sum1 + ut * WinSampl[q - q1 + 1] * 2.71828183 ** (
                 -4j * np.pi / lamda * ar / 2 * ((q - q0) * Tr) ** 2)
         Zxy1[nx, ny] = sum1
 
 
+@cuda.jit
+def kernel_2d_array_2(X1sint, Y1sint, dxsint, dysint, Xrls, Yrls, Zrls,
+               Vxrls, Vyrls, Vzrls, Tsint, Tr, Vrls, L, c, t2, T0, Kss,
+               Uout01ss, Fs, WinSampl, Zxy1, Zxy2, lamda, Uout02ss):
+    e = 2.71828183
+    nx, ny = cuda.grid(2)
+            # координаты текущей точки наблюдения
+    if nx < Zxy1.shape[0] and ny < Zxy1.shape[1]:
+        xt = X1sint + nx * dxsint
+        yt = Y1sint + ny * dysint
+        zt = 0
+        # определяем интервал индексов отсчетов для суммирования отсчетов исходя из
+        # нахождения на траверсе для передающего канала
+        q0 = int((yt - Yrls[0]) / (
+                Vyrls * Tr))  # индекс номера периода повторения для траверса
+        q1 = q0 - int(Tsint / 2 / Tr)  # начальный индекс суммирования
+        q2 = q0 + int(Tsint / 2 / Tr)  # конечный индекс суммирования
+        d0 = ((Xrls[0] - xt) ** 2 + (
+                Zrls[0] - zt) ** 2) ** 0.5 # дальность на траверсе
+
+        ar = Vrls ** 2 / d0  # радиальное ускорение для компенсации МД и МЧ
+        # нескомпенсированные скорости для приемных каналов
+        Vr1 = L / 2 / d0 * Vrls
+        Vr2 = -L / 2 / d0 * Vrls
+        # непосредственно суммирование - КН с компенсацией МД и МЧ
+        sum1 = 0
+        sum2 = 0
+        for q in range(q1, q2):
+            d = ((xt - (Xrls[0] + Vxrls * q * Tr)) ** 2 +
+                 (yt - (Yrls[0] + Vyrls * q * Tr)) ** 2 +
+                 (zt - (Zrls[0] + Vzrls * q * Tr)) ** 2) ** 0.5
+            ndr = (2 * d / c - t2 + T0) * Kss * Fs  # дробный номер отсчета по быстрому времени
+            n = int(ndr) + 1
+            drob = ndr % 1
+            ut = Uout01ss[n, q] * (1 - drob) + Uout01ss[n + 1, q] * drob
+            # суммируем с учетом сдвига РЛИ по скорости
+            sum1 = sum1 + ut * WinSampl[q - q1 + 1] * e ** (
+                -1j * 4 * np.pi / lamda * ar / 2 * (
+                            (q - q0) * Tr) ** 2) * e ** (
+                1j * 2 * np.pi * Vr1 / lamda * (q - q0) * Tr)
+            ut = Uout02ss[n, q] * (1 - drob) + Uout02ss[n + 1, q] * drob
+            sum2 = sum2 + ut * WinSampl[q - q1 + 1] * e ** (
+                -1j * 4 * np.pi / lamda * ar / 2 * (
+                            (q - q0) * Tr) ** 2) * e ** (
+                1j * 2 * np.pi * Vr2 / lamda * (q - q0) * Tr)
+        Zxy1[nx, ny] = sum1
+        Zxy2[nx, ny] = sum2
+
+
+@time_of_function
 def big_cycle1(X1sint, Y1sint, dxsint, dysint, Xrls, Yrls, Zrls,
                Vxrls, Vyrls, Vzrls, Tsint, Tr, Vrls, c, t2, T0, Kss,
                Uout01ss, Fs, WinSampl, Zxy1, lamda):
-    threads_per_block = (32, 32)
+    threads_per_block = (16, 16)
     blocks_per_grid_x = math.ceil(Zxy1.shape[0] / threads_per_block[0])
     blocks_per_grid_y = math.ceil(Zxy1.shape[1] / threads_per_block[1])
     blocks_per_grid = (blocks_per_grid_x, blocks_per_grid_y)
-    kernel_2d_array[blocks_per_grid, threads_per_block](X1sint, Y1sint, dxsint, dysint, Xrls, Yrls, Zrls,
+    kernel_2d_array_1[blocks_per_grid, threads_per_block](X1sint, Y1sint, dxsint, dysint, Xrls, Yrls, Zrls,
                                                         Vxrls, Vyrls, Vzrls, Tsint, Tr, Vrls, c, t2, T0, Kss,
                                                         Uout01ss, Fs, WinSampl, Zxy1, lamda)
     return Zxy1
+
+@time_of_function
+def big_cycle2(X1sint, Y1sint, dxsint, dysint, Xrls, Yrls, Zrls,
+               Vxrls, Vyrls, Vzrls, Tsint, Tr, Vrls, L, c, t2, T0, Kss,
+               Uout01ss, Fs, WinSampl, Zxy1, Zxy2, lamda, Uout02ss):
+    threads_per_block = (8, 8)
+    blocks_per_grid_x = math.ceil(Zxy1.shape[0] / threads_per_block[0])
+    blocks_per_grid_y = math.ceil(Zxy1.shape[1] / threads_per_block[1])
+    blocks_per_grid = (blocks_per_grid_x, blocks_per_grid_y)
+    kernel_2d_array_2[blocks_per_grid, threads_per_block](X1sint, Y1sint, dxsint, dysint, Xrls, Yrls, Zrls,
+               Vxrls, Vyrls, Vzrls, Tsint, Tr, Vrls, L, c, t2, T0, Kss,
+               Uout01ss, Fs, WinSampl, Zxy1, Zxy2, lamda, Uout02ss)
+    return Zxy1, Zxy2

main.py
@@ -17,15 +17,15 @@ from calc_rli_xy_ss import (add_zeros,
                             add_zeros_2_channels,
                             big_cycle1,
                             big_cycle2)
-from gpu_rli_xy_ss import big_cycle1
+from gpu_rli_xy_ss import big_cycle1, big_cycle2
 
-time_prev= time.time()
-# общие переменные
+time_prev = time.time()
+# общие переменные-
 c = 3 * 10 ** 8
 df = 25 * 10 ** 6  # ширина спектра зондирующего сигнала *********
 Ks = 1  # коэффициент Найквиста для исходного сигнала
-Kss = 1  # коэффициент передискретизации *****
-T0 = 10 ** -4  # длительность импульса
+Kss = 4  # коэффициент передискретизации *****
+T0 = 10 ** -5  # длительность импульса
 Fs = Ks * df  # частота дискретизации
 T = 4 * T0  # длительность моделируемого участка
 N = int(T * Fs)  # число отчетов в пределах периода повторения
@@ -37,7 +37,7 @@ lamda = 0.03  # длина волны
 f0 = c / lamda  # несущая частота
 
 Tn = 1.0  # общее время наблюдения
-Tr = 0.25 * 10 ** -2  # период повторения ******
+Tr = 0.25 * 10 ** -3  # период повторения ******
 Q = int(Tn / Tr)  # число импульсов в пачке
 Tsint = 0.4  # время синтезирования
 
@@ -322,7 +322,7 @@ if FlagInter == 0:
                Uout01ss, Fs, WinSampl, Zxy1, lamda)
 if FlagInter == 1:
     Zxy2 = np.zeros((Nxsint, Nysint), complex)
-    Zxy1, Zxy2 = big_cycle2(Nxsint, Nysint, X1sint, Y1sint, dxsint, dysint, Xrls, Yrls, Zrls,
+    Zxy1, Zxy2 = big_cycle2(X1sint, Y1sint, dxsint, dysint, Xrls, Yrls, Zrls,
                Vxrls, Vyrls, Vzrls, Tsint, Tr, Vrls, L, c, t2, T0, Kss,
                Uout01ss, Fs, WinSampl, Zxy1, Zxy2, lamda, Uout02ss)
 