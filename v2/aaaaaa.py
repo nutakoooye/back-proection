57414b16b48a346900e1290cd0100e36715825f2
Konstantin Krytski <kostyalom321z@gmail.com>
2023-05-23 13:56:34 +0300

    ghcfgh

---
+++ v2/aaaaaa.py
@@ -0,0 +1,19 @@
+
+def test_main(client_values):
+    Kss = client_values['Kss']
+    dxsint = client_values['dxsint']
+    dysint = client_values['dysint']
+    StepBright = client_values['StepBright']
+    Nxsint = client_values['Nxsint']
+    Nysint = client_values['Nysint']
+    Tsint = client_values['Tsint']
+    tauRli = client_values['tauRli']
+    RegimRsa = client_values['RegimRsa']
+    TypeWinDp = client_values['TypeWinDp']
+    TypeWinDn = client_values['TypeWinDn']
+    isGPU = client_values['isGPU']
+    FlagViewSignal = client_values['FlagViewSignal']
+
+
+
+# 't_r_w': float(ui.findChild(QDoubleSpinBox, "t_r_w").text().replace(',', '.')),  # мб не то :)
